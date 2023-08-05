61b6f452388f82e40205d536b671e91ca10f7e59
Nikola Milosevic <nikolamilosevic441@gmail.com>
2023-08-05 11:53:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VSCODE C++ Project Setup
+
+Sets up a C++17 project with:
+- CMake for building
+- Catch2 for testing
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stb_image.h>
+
+int main() {
+    int i = 9;
+    std::cout << "Hello, "<<i<<"!" << std::endl;
+    return 0;
+}
